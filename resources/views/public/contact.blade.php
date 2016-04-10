d4103cbad6070f9fc03aa88e7c21f67b32ef76aa
sulthanallaudeen <sa@sysaxiom.com>
2016-04-10 14:14:20 +0530

    updated reume content

--- resources/views/public/contact.blade.php
+++ resources/views/public/contact.blade.php
@@ -1,81 +1,77 @@
-<?php
-
-namespace App;
-
-use App\Blog;
-use URL;
-use Form; ?>
+<?php namespace App; use App\Blog; use URL; use Form;?>
 @extends('layout.public')
 @section('content')
 <div class="row">
-    <div class="col-lg-8">
-        <div class="jumbotron" style="background-color: #F0563D">
-            <p style="text-align:center">     
-                I can be reached via email at <br><b>sa (SHIFT+2) sulthanallaudeen (period) com  </b>*
-            </p>
-            <br>
+        <div class="col-lg-8">
+<div class="jumbotron" style="background-color: #F0563D">
+   <p style="text-align:center">     
+I can be reached via email at <br><b>sa (SHIFT+2) sulthanallaudeen (period) com  </b>*
+</p>
+<br>
 
-            * If you are a human, you should be able to decipher the email address.
-        </div>
-        <div class="well" style="background-color: white">
-            <div class="alert alert-success" id="mailSentSucess" style="display:none"><span id="mailResultSuccess"</span></div>
-            <div class="alert alert-danger" id="mailSentFailure" style="display:none"><span id="mailResultFailure"</span></div>
-            <div id="formContent">
-                {!! Form::open(array('class' => 'form-horizontal' )) !!}
-                <input type="hidden", "_token"  name="_token" value="{{ csrf_token()}}" >
-                <h2>Email Me</h2>
-                <input type="text" id="userEmail" class="form-control" placeholder="Email address">
-                <textarea class="form-control" id="userMessage" rows="3" placeholder="Message"></textarea>
-                <button class="btn btn-lg btn-primary btn-block" type="submit" id="sendMail">Send</button>
-                {!! Form::close() !!}
+* If you are a human, you should be able to decipher the email address.
+      </div>
+      <div class="well" style="background-color: white">
+      <div class="alert alert-success" id="mailSentSucess" style="display:none"><span id="mailResultSuccess"</span></div>
+      <div class="alert alert-danger" id="mailSentFailure" style="display:none"><span id="mailResultFailure"</span></div>
+    <div id="formContent">
+    {!! Form::open(array('class' => 'form-horizontal' )) !!}
+    <input type="hidden", "_token"  name="_token" value="{{ csrf_token()}}" >
+            <h2>Email Me</h2>
+        <input type="text" id="userEmail" class="form-control" placeholder="Email address">
+        <textarea class="form-control" id="userMessage" rows="3" placeholder="Message"></textarea>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" id="sendMail">Send</button>
+      {!! Form::close() !!}
+      </div>
+      </div>
+            
+
             </div>
-        </div>
-
-
-    </div>
-    <div class="col-md-4">
-
+        <div class="col-xs-12 col-sm-4">
+        <!-- CONTACT -->
         <div class="box clearfix">
             <h2>Contact</h2>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-phone fa-fw"></span></div>
-              <div class="title only pull-right" id="adminmobile"></div>
+                <div class="icon pull-left text-center"><span class="fa fa-phone fa-fw"></span></div>
+                <div class="title only pull-right" id="adminmobile"></div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-envelope fa-fw"></span></div>
-              <div class="title only pull-right" id="adminemail"></div>
+                <div class="icon pull-left text-center"><span class="fa fa-envelope fa-fw"></span></div>
+                <div class="title only pull-right" id="adminemail"></div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-github fa-fw"></span></div>
-              <div class="title only pull-right" id="admingithub"></div>
+                <div class="icon pull-left text-center"><span class="fa fa-github fa-fw"></span></div>
+                <div class="title only pull-right" id="admingithub"></div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-stack-overflow fa-fw"></span></div>
-              <div class="title only pull-right" id="adminstackoverflow"></div>
+                <div class="icon pull-left text-center"><span class="fa fa-stack-overflow fa-fw"></span></div>
+                <div class="title only pull-right" id="adminstackoverflow"></div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-twitter fa-fw"></span></div>
-              <div class="title pull-right">Twitter</div>
-              <div class="description pull-right" id='admintwitter'></div>
+                <div class="icon pull-left text-center"><span class="fa fa-twitter fa-fw"></span></div>
+                <div class="title pull-right">Twitter</div>
+                <div class="description pull-right" id='admintwitter'></div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-facebook fa-fw"></span></div>
-              <div class="title pull-right">Facebook</div>
-              <div class="description pull-right" id='adminfacebook'></div>
+                <div class="icon pull-left text-center"><span class="fa fa-facebook fa-fw"></span></div>
+                <div class="title pull-right">Facebook</div>
+                <div class="description pull-right" id='adminfacebook'></div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-skype fa-fw"></span></div>
-              <div class="title pull-right">Skype</div>
-              <div class="description pull-right" id="adminskype"></div>
+                <div class="icon pull-left text-center"><span class="fa fa-skype fa-fw"></span></div>
+                <div class="title pull-right">Skype</div>
+                <div class="description pull-right" id="adminskype"></div>
             </div>
-          </div>
+        </div>
         <!-- SKILLS -->
         <div class="box">
             <h2>Skills</h2>
             <div class="skills">
                 <div class="item-skills" data-percent="1.00">Laravel (framework)</div>
                 <div class="item-skills" data-percent="0.90">PHP</div>
-                <div class="item-skills" data-percent="0.70">HTML</div>
+                <div class="item-skills" data-percent="0.80">MySQL</div>
+                <div class="item-skills" data-percent="0.85">JQuery</div>
+                <div class="item-skills" data-percent="0.75">AngularJS</div>
                 <div class="skills-legend clearfix">
                     <div class="legend-left legend">Beginner</div>
                     <div class="legend-left legend"><span>Proficient</span></div>
@@ -84,81 +80,73 @@ use Form; ?>
                 </div>
             </div>
         </div>
-        <!-- LANGUAGES -->
-        <div class="box">
-            <h2>Languages</h2>
-            <div id="language-skills">
-                <div class="skill">Polish <div class="icons pull-right"><div style="width: 80%;" class="icons-red"></div></div></div>
-                <div class="skill">English <div class="icons pull-right"><div style="width: 60%;" class="icons-red"></div></div></div>
-            </div>
-        </div>
         <!-- HOBBIES -->
         <div class="box">
             <h2>Hobbies</h2>
+            <div class="hobby">Programming</div>
             <div class="hobby">Photography</div>
             <div class="hobby">Fitness</div>
-            <div class="hobby">Programming</div>
         </div>
-
     </div>
-</div>
-</div>
+      </div>
+    </div>
 <script>
-    $(document).ready(function () {
+$(document).ready(function() {
+
+  
+  $( "#sendMail" ).click(function() {
+  event.preventDefault();
+  var _token = $("input[name=_token]").val();
+  var userEmail = $("#userEmail").val();
+  var userMessage = $("#userMessage").val();
+  $.post( "sendMail", { _token : _token, userEmail : userEmail, userMessage : userMessage })
+  .done(function( data ) {
+    var result = jQuery.parseJSON(JSON.stringify(data));
+    if (result.success==1)
+    { 
+     $("#mailSentFailure").hide();
+     $("#formContent").hide();
+     $("#mailResultSuccess").html("Mail Sent Succesfully.. Mail Id : "+result.mailId);
+     $("#mailSentSucess").show();
+
+    }
+    else
+    {
+    
+     $("#mailResultFailure").html("");
+     $("#mailResultFailure").html(result.error.userEmail);
+     $("#mailResultFailure").append(result.error.userMessage);
+     $("#mailSentFailure").show();
+
+    }
+
+  });
+});
 
 
-        $("#sendMail").click(function () {
-            event.preventDefault();
-            var _token = $("input[name=_token]").val();
-            var userEmail = $("#userEmail").val();
-            var userMessage = $("#userMessage").val();
-            $.post("sendMail", {_token: _token, userEmail: userEmail, userMessage: userMessage})
-                    .done(function (data) {
-                        var result = jQuery.parseJSON(JSON.stringify(data));
-                        if (result.success == 1)
-                        {
-                            $("#mailSentFailure").hide();
-                            $("#formContent").hide();
-                            $("#mailResultSuccess").html("Mail Sent Succesfully.. Mail Id : " + result.mailId);
-                            $("#mailSentSucess").show();
-
-                        } else
-                        {
-
-                            $("#mailResultFailure").html("");
-                            $("#mailResultFailure").html(result.error.userEmail);
-                            $("#mailResultFailure").append(result.error.userMessage);
-                            $("#mailSentFailure").show();
-
-                        }
-
-                    });
-        });
-
-
-    });
+} );
 
 </script>
 <script>
-    $(document).ready(function () {
-        $("#s").click(function () {
-            var _token = $("input[name=_token]").val();
-            event.preventDefault();
-            var emailSender = $("#userEmail").val();
-            var userMessage = $("#userMessage").val();
-            alert(_token);
-            alert(emailSender);
-            alert(userMessage);
-            $.post("postTag", {_token: _token, userEmail: userEmail, userMessage: userMessage})
-                    .done(function (data) {
-                        alert(data);
-                        $(".well").html("");
-                        $(".mailResult").html("Mail Sent Succesfully");
-                    });
+$( document ).ready(function() {
+  $( "#s" ).click(function() {
+    var _token = $("input[name=_token]").val();
+    event.preventDefault();
+var emailSender = $("#userEmail").val();
+var userMessage = $("#userMessage").val();
+alert(_token);
+alert(emailSender);
+alert(userMessage);
+$.post( "postTag", { _token : _token, userEmail : userEmail, userMessage : userMessage })
+  .done(function( data ) {
+    alert(data);
+    $(".well").html("");
+    $(".mailResult").html("Mail Sent Succesfully");
+  });
 
 
-
-        });
-    });
+    
+});
+});
 </script>
 @stop
--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,149 +1,142 @@
 @extends('layout.public')
 @section('content')
 <div class="row">
-        <div class="col-xs-12 col-sm-8">
-          <!-- ABOUT ME -->
-          <div class="box">
+    <div class="col-xs-12 col-sm-8">
+        <!-- ABOUT ME -->
+        <div class="box">
+            <h2>Introduction</h2>
+            <p>I am a <b>Full Stack Developer</b>. I live in <b>Chennai</b> - India. I graduated <b>
+                    Masters in Computer science</b> at Vel Tech Engineering College, Chennai. 
+                At this site you can find information what I do at the moment, what's my current experience and skills and which skills I don't have (yet). 
+                You may also be interested to view my profile at <b><a href="http://stackoverflow.com/users/3282633" target="_new">Stackoverflow</a></b>.</p>
+        </div>
+        <div class="box">
             <h2>About Me</h2>
-            <p>Software Engineer who loves Rapid and Serious Web Applications and Services mostly in PHP.</p>
-          </div>
-         
-          <!-- EXPERIENCES -->
-          <div class="box">
+            <p>I have over 2.8 years experience in programming and in that time I had opportunity to learn technologies that are useful at websites and web applications development. The most important ones are PHP, Laravel framework, MySQL, JQuery, AWS and GIT. I have more interest on writing web services for across platforms, also follows Agile</p>
+        </div>
+        <!-- EXPERIENCES -->
+        <div class="box">
             <h2>Experiences</h2>
             <div class="job clearfix">
-              <div class="col-xs-3">
-                <div class="where">Wekancode</div>
-                <div class="year">2015 - Till Date</div>
-              </div>
-              <div class="col-xs-9">
-                <div class="profession">Full Stack Developer</div>
-                <div class="description">Developed an Application :: GreenHopping - A Juice Ordering System. Developed Front End, Back End, Web Services with Admin Panel hosted on AWS.</div>
-              </div>
+                <div class="col-xs-3">
+                    <div class="where">Wekancode</div>
+                    <div class="year">2015 - Till Date</div>
+                </div>
+                <div class="col-xs-9">
+                    <div class="profession">Full Stack Developer</div>
+                    <div class="description">Developed an Application :: <b><a href='greenhoppingapp.com' target="_new">GreenHopping</a></b> - A Juice Ordering System. Developed Front End, Back End, Web Services with Admin Panel hosted on AWS. Also, developing an application for <a href="testmyblood.in" target="_new">Medical based activities</a></b>.</div>
+                </div>
             </div>
             <div class="job clearfix">
-              <div class="col-xs-3">
-                <div class="where">Bizarre Software Solutions</div>
-                <div class="year">2014-2015</div>
-              </div>
-              <div class="col-xs-9">
-                <div class="profession">BackEnd Developer</div>
-                <div class="description">Developed MTI, Masna Taxi and Hashtag Pizza. Mostly written webservices for Android and iOS Devices using Google Cloud Messages and Apple Push Notifiatoin System. Tweaked on Hybrid Application on Android and Windows</div>
-              </div>
+                <div class="col-xs-3">
+                    <div class="where">Bizarre Software Solutions</div>
+                    <div class="year">2014-2015</div>
+                </div>
+                <div class="col-xs-9">
+                    <div class="profession">BackEnd Developer</div>
+                    <div class="description">Developed <a href="http://api.taxihail.com/MaldenTransInc" target="_new">MTI</a>, Masna Taxi and Hashtag Pizza. Also written webservices for Android and iOS Devices using Google Cloud Messages and Apple Push Notifiatoin System. Tweaked on Hybrid Application on Android and Windows</div>
+                </div>
             </div>
             <div class="job clearfix">
-              <div class="col-xs-3">
-                <div class="where">Mind Ocean Technologies</div>
-                <div class="year">2013-2014</div>
-              </div>
-              <div class="col-xs-9">
-                <div class="profession">BackEnd Developer</div>
-                <div class="description">Written few modules for Learning Management System named as E-Future and developed many functions for Real Estate Portal named as Earthzu.</div>
-              </div>
+                <div class="col-xs-3">
+                    <div class="where">Mind Ocean Technologies</div>
+                    <div class="year">2013-2014</div>
+                </div>
+                <div class="col-xs-9">
+                    <div class="profession">PHP Developer</div>
+                    <div class="description">Written few modules for Learning Management System named as E-Future and developed many functions for Real Estate Portal named as Earthzu.</div>
+                </div>
             </div>
-          </div>
-           <!-- EDUCATION -->
-          <div class="box">
+        </div>
+        <!-- EDUCATION -->
+        <div class="box">
             <h2>Education</h2>
             <ul id="education" class="clearfix">
-              <li>
-                <div class="year pull-left">1990</div>
-                <div class="description pull-right">
-                  <h3>I was born in Dindigul</h3>
-                  <p>Did the basics school in Om Shanthi CBSE School which thought me the basic logical thinking and lot of stuffs.</p>
-                </div>
-              </li>
-              <li>
-                <div class="year pull-left">2007</div>
-                <div class="description pull-right">
-                  <h3>Schooling and handling Computing Machine</h3>
-                  <p>Early education with computers was done while schooling. The self interest on handling computing machine does a lot</p>
-                </div>
-              </li>
-              <li>
-                <div class="year pull-left">2005</div>
-                <div class="description pull-right">
-                  <h3>Bachelors Graduation in Computer Science</h3>
-                  <p>Completed the Bachelors Degree in Computer Science in Rockfort City (Trichy) - JMC. Participated in NCC and attended national level camps - RDC</p>
-                </div>
-              </li>
-              <li>
-                <div class="year pull-left">2009</div>
-                <div class="description pull-right">
-                  <h3>Masters Graduation in Computer Science</h3>
-                  <p>Did the Masters in Computer Applicaton at Vel Tech Engineering College, Chennai. Learned the awesome language PHP and developed several applications for personal and public usage.</p>
-                </div>
-              </li>
+                <li>
+                    <div class="year pull-left">2013</div>
+                    <div class="description pull-right">
+                        <h3>Masters Graduation in Computer Science</h3>
+                        <p>Did the Masters in Computer Applicaton at Vel Tech Engineering College, Chennai. Started with the awesome language PHP and developed several applications for personal and public usage.</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="year pull-left">2010</div>
+                    <div class="description pull-right">
+                        <h3>Bachelors Graduation in Computer Science</h3>
+                        <p>Completed the Bachelors Degree in Computer Science in Rockfort City (Trichy)</p>
+                    </div>
+                </li>
+                <li>
+                    <div class="year pull-left">2007</div>
+                    <div class="description pull-right">
+                        <h3>Schooling and handling Computing Machine</h3>
+                        <p>Early education with computers was initiated during schooling, the place which thought me handling computing machine, logical thinking etc.,</p>
+                    </div>
+                </li>            
             </ul>
-          </div>
         </div>
-        <div class="col-xs-12 col-sm-4">
-          <!-- CONTACT -->
-          <div class="box clearfix">
+    </div>
+    <div class="col-xs-12 col-sm-4">
+        <!-- CONTACT -->
+        <div class="box clearfix">
             <h2>Contact</h2>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-phone fa-fw"></span></div>
-              <div class="title only pull-right" id="adminmobile"></div>
+                <div class="icon pull-left text-center"><span class="fa fa-phone fa-fw"></span></div>
+                <div class="title only pull-right" id="adminmobile"></div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-envelope fa-fw"></span></div>
-              <div class="title only pull-right" id="adminemail"></div>
+                <div class="icon pull-left text-center"><span class="fa fa-envelope fa-fw"></span></div>
+                <div class="title only pull-right" id="adminemail"></div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-github fa-fw"></span></div>
-              <div class="title only pull-right" id="admingithub"></div>
+                <div class="icon pull-left text-center"><span class="fa fa-github fa-fw"></span></div>
+                <div class="title only pull-right" id="admingithub"></div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-stack-overflow fa-fw"></span></div>
-              <div class="title only pull-right" id="adminstackoverflow"></div>
+                <div class="icon pull-left text-center"><span class="fa fa-stack-overflow fa-fw"></span></div>
+                <div class="title only pull-right" id="adminstackoverflow"></div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-twitter fa-fw"></span></div>
-              <div class="title pull-right">Twitter</div>
-              <div class="description pull-right" id='admintwitter'></div>
+                <div class="icon pull-left text-center"><span class="fa fa-twitter fa-fw"></span></div>
+                <div class="title pull-right">Twitter</div>
+                <div class="description pull-right" id='admintwitter'></div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-facebook fa-fw"></span></div>
-              <div class="title pull-right">Facebook</div>
-              <div class="description pull-right" id='adminfacebook'></div>
+                <div class="icon pull-left text-center"><span class="fa fa-facebook fa-fw"></span></div>
+                <div class="title pull-right">Facebook</div>
+                <div class="description pull-right" id='adminfacebook'></div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><span class="fa fa-skype fa-fw"></span></div>
-              <div class="title pull-right">Skype</div>
-              <div class="description pull-right" id="adminskype"></div>
+                <div class="icon pull-left text-center"><span class="fa fa-skype fa-fw"></span></div>
+                <div class="title pull-right">Skype</div>
+                <div class="description pull-right" id="adminskype"></div>
             </div>
-          </div>
-          <!-- SKILLS -->
-          <div class="box">
+        </div>
+        <!-- SKILLS -->
+        <div class="box">
             <h2>Skills</h2>
             <div class="skills">
-              <div class="item-skills" data-percent="1.00">Laravel (framework)</div>
-              <div class="item-skills" data-percent="0.90">PHP</div>
-              <div class="item-skills" data-percent="0.70">HTML</div>
-              <div class="skills-legend clearfix">
-                <div class="legend-left legend">Beginner</div>
-                <div class="legend-left legend"><span>Proficient</span></div>
-                <div class="legend-right legend"><span>Expert</span></div>
-                <div class="legend-right legend">Master</div>
-              </div>
+                <div class="item-skills" data-percent="1.00">Laravel (framework)</div>
+                <div class="item-skills" data-percent="0.90">PHP</div>
+                <div class="item-skills" data-percent="0.80">MySQL</div>
+                <div class="item-skills" data-percent="0.85">JQuery</div>
+                <div class="item-skills" data-percent="0.75">AngularJS</div>
+                <div class="skills-legend clearfix">
+                    <div class="legend-left legend">Beginner</div>
+                    <div class="legend-left legend"><span>Proficient</span></div>
+                    <div class="legend-right legend"><span>Expert</span></div>
+                    <div class="legend-right legend">Master</div>
+                </div>
             </div>
-          </div>
-          <!-- LANGUAGES -->
-          <div class="box">
-            <h2>Languages</h2>
-            <div id="language-skills">
-              <div class="skill">Polish <div class="icons pull-right"><div style="width: 80%;" class="icons-red"></div></div></div>
-              <div class="skill">English <div class="icons pull-right"><div style="width: 60%;" class="icons-red"></div></div></div>
-            </div>
-          </div>
-          <!-- HOBBIES -->
-          <div class="box">
+        </div>
+        <!-- HOBBIES -->
+        <div class="box">
             <h2>Hobbies</h2>
+            <div class="hobby">Programming</div>
             <div class="hobby">Photography</div>
             <div class="hobby">Fitness</div>
-            <div class="hobby">Programming</div>
-          </div>
         </div>
-      </div>
     </div>
+</div>
+</div>
 @endsection
