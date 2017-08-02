5147dad26441ca8e37757d620ea4a9c44ae6dd2e
sulthanallaudeen <sa@sulthanallaudeen.com>
2017-08-02 21:36:42 +0530

    Temporarily Disabling SSL Certificate Verification as i need to run in Local Machine

--- cron.php
+++ cron.php
@@ -4,6 +4,13 @@ $url = $_SERVER['REQUEST_URI'];
 $url = substr($url, 0, strrpos( $url, '/'));
 $url = $host.$url;
 $url = "https://".$url."/cron";
-$data = file_get_contents($url);
+#Temporarily Disabling SSL Certificate Verification as i need to run in Local Machine
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+);
+$data = file_get_contents($url, false, stream_context_create($arrContextOptions));
 print_r($data);
 ?>
