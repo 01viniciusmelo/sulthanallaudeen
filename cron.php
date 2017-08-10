fe814cc2ec38438fb94ae1dfb7049170a8e5c4c5
sulthanallaudeen <sa@sulthanallaudeen.com>
2017-08-10 16:18:21 +0530

    Auto URL Locator

--- cron.php
+++ cron.php
@@ -1,25 +1,26 @@
- <?php
- //print_r($_SERVER);
- echo $_SERVER['HTTP_HOST'];
- exit;
- $txt = 'Cron on '.date("Y-m-d H:i:s").'|';
- $fileName = 'public/logs/cron-'.date("Y-m-d-H").'.txt';
- file_put_contents($fileName, $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
- ?>
 <?php
-// $host = $_SERVER['HTTP_HOST'];
-// $url = $_SERVER['REQUEST_URI'];
-// $url = substr($url, 0, strrpos( $url, '/'));
-// $url = $host.$url;
-// $url = "https://".$url."/cron";
-// #Temporarily Disabling SSL Certificate Verification as i need to run in Local Machine
-// $arrContextOptions=array(
-//     "ssl"=>array(
-//         "verify_peer"=>false,
-//         "verify_peer_name"=>false,
-//     ),
-// );
-// $data = file_get_contents($url, false, stream_context_create($arrContextOptions));
-// $data = file_get_contents($url);
-// print_r($data);
+    $host = $_SERVER['HTTP_HOST'];
+    $url = $_SERVER['REQUEST_URI'];
+    $url = substr($url, 0, strrpos( $url, '/'));
+    $url = $host.$url;
+    if($host=='localhost'){
+        $url = "https://".$url."/cron";
+        #Temporarily Disabling SSL Certificate Verification as i need to run in Local Machine
+        $arrContextOptions=array(
+        "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+        ),
+        );
+        $data = file_get_contents($url, false, stream_context_create($arrContextOptions));
+    }
+    else{
+        $data = file_get_contents($url);
+    }
+/*Temporary Cron Entry 
+*/
+$txt = 'Cron on '.date("Y-m-d H:i:s").'|';
+$fileName = 'public/logs/cron-'.date("Y-m-d-H").'.txt';
+file_put_contents($fileName, $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
+
 ?>
