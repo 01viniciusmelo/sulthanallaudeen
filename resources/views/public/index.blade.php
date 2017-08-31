5a49aee6281aaf733cadcd5bad93bf709743bdb5
sulthanallaudeen <sa@sulthanallaudeen.com>
2017-08-31 06:44:59 +0530

    Home Page Almost Done

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -41,21 +41,41 @@
     </nav>
 
     <div class="container">
-      <div class="jumbotron">
-        <p class="sa-img-box"><img src="{{ asset('/').('public/images/sulthan-allaudeen.jpg') }}" class="img-fluid rounded-circle sa-img" alt="Sulthan Allaudeen"> <h1 class="sa-name">Sulthan Allaudeen</h1></p>
+      <div class="jumbotron sa-home-card">
+        <p class="sa-img-box"><img src="{{ asset('/').('public/images/sulthan-allaudeen.jpg') }}" class="img-fluid rounded-circle sa-img" alt="Sulthan Allaudeen"> <h1 class="sa-name">Sulthan Allaudeen</h1>
          <div class="row">
           <div class="col-4">
-            <a href="https://github.com/sulthanallaudeen" class="sa-social-lnk" target="_new"><i class="fa fa-github sa-fa-s-lnk" aria-hidden="true"></i></a>
+            <a href="https://github.com/sulthanallaudeen" class="sa-social-lnk" target="_blank"><i class="fa fa-github sa-fa-s-lnk" aria-hidden="true"></i></a>
           </div>
           <div class="col-4">
-            <a href="https://stackoverflow.com/users/3282633/sulthan-allaudeen" class="sa-social-lnk" target="_new"><i class="fa fa-stack-overflow sa-fa-s-lnk" aria-hidden="true"></i></a>
+            <a href="https://stackoverflow.com/users/3282633/sulthan-allaudeen" class="sa-social-lnk" target="_blank"><i class="fa fa-stack-overflow sa-fa-s-lnk" aria-hidden="true"></i></a>
           </div>
           <div class="col-4">
-            <a href="https://www.linkedin.com/in/sulthanallaudeen/" class="sa-social-lnk" target="_new"><i class="fa fa-linkedin-square sa-fa-s-lnk" aria-hidden="true"></i></a>
+            <a href="https://www.linkedin.com/in/sulthanallaudeen/" class="sa-social-lnk" target="_blank"><i class="fa fa-linkedin-square sa-fa-s-lnk" aria-hidden="true"></i></a>
           </div>
         </div>
       </div>
     </div>
+
+    <div class="container">
+         <div class="row">
+           <div class="col-3">
+            <ul class="list-unstyled">
+              <li>&copy; 2010 - 2017 <a rel="license" target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png" class="cc-img"/></a></li>
+              <li><i class="fa fa-envelope fa-fw"></i> <a href="mailto:sa@sulthanallaudeen.com">sa@sulthanallaudeen.com</a></li>
+              <li><i class="fa fa-phone-square fa-fw"></i> 904.244.5010</li>
+            </ul>
+          </div>
+          <div class="col-6">
+            <p class="text-muted text-center">Created with <i class="fa fa-heart" aria-hidden="true"></i>, using <a href="http://www.laravel.com/" target="_blank">Laravel </a>! <br>(With the help of <a href="http://getbootstrap.com" target="_blank">Twitter Bootstrap</a> and <a href="http://fontawesome.io" target="_blank">Font Awesome</a>)</p>
+          </div>
+          <div class="col-3">
+            <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fsulthanallaudeen.com%2F" target="blank"><img src="https://www.w3.org/Icons/valid-html20-blue.png"></a>
+            <span class='pull-right' id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=zXDi8lx071OVItgGihdEfagLQo0YfNmUadOX0spepLVhyMH3AFGUPxDxTrka"></script></span>
+          </div>
+        </div>
+    </div>
+    
 
 
 
