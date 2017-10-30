9f9a56f9995b12811ea94973a3a01347f2f3a8bc
sulthanallaudeen <sa@sulthanallaudeen.com>
2017-10-30 18:27:28 +0530

    Modified .htaccess

--- cron.php
+++ cron.php
@@ -1,10 +1,10 @@
 <?php
 
-    $host = $_SERVER['HTTP_HOST'];
-    $url = $_SERVER['REQUEST_URI'];
-    $url = substr($url, 0, strrpos( $url, '/'));
-    $url = $host.$url;
-    $localName = 'sulthanallaudeen';
+    // $host = $_SERVER['HTTP_HOST'];
+    // $url = $_SERVER['REQUEST_URI'];
+    // $url = substr($url, 0, strrpos( $url, '/'));
+    // $url = $host.$url;
+    // $localName = 'sulthanallaudeen';
     //$data = file_get_contents("https://".$url."/cron");
     $data = file_get_contents("https://sulthanallaudeen.com/cron");
     // if($host=='localhost'){
