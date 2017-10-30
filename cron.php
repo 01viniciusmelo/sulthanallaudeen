b33255f1875eba3b8ffc5f84f09e966ccbacbb44
sulthanallaudeen <sa@sulthanallaudeen.com>
2017-10-30 18:17:20 +0530

    Modified cron

--- cron.php
+++ cron.php
@@ -5,19 +5,20 @@
     $url = substr($url, 0, strrpos( $url, '/'));
     $url = $host.$url;
     $localName = 'sulthanallaudeen';
-    if($host=='localhost'){
-        $url = "https://".$url."/cron";
-        #Temporarily Disabling SSL Certificate Verification as i need to run in Local Machine
-        $arrContextOptions=array(
-        "ssl"=>array(
-        "verify_peer"=>false,
-        "verify_peer_name"=>false,
-        ),
-        );
-        $data = file_get_contents($url, false, stream_context_create($arrContextOptions));
-    }
-    else{
-        $data = file_get_contents("https://".$url."/cron");
-    }
+    $data = file_get_contents("https://".$url."/cron");
+    // if($host=='localhost'){
+    //     $url = "https://".$url."/cron";
+    //     #Temporarily Disabling SSL Certificate Verification as i need to run in Local Machine
+    //     $arrContextOptions=array(
+    //     "ssl"=>array(
+    //     "verify_peer"=>false,
+    //     "verify_peer_name"=>false,
+    //     ),
+    //     );
+    //     $data = file_get_contents($url, false, stream_context_create($arrContextOptions));
+    // }
+    // else{
+        
+    // }
 echo $host.$data;
 ?>
