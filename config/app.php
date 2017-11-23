1f7012a5c57a0ff49f360228f4a7161c78207526
sulthanallaudeen <sa@sulthanallaudeen.com>
2017-11-23 08:24:31 +0530

    Edited Conf

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://www.sulthanallaudeen.com'),
+    'url' => env('APP_URL', 'https://sulthanallaudeen.com'),
 
     /*
     |--------------------------------------------------------------------------
